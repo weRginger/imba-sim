e9cbb3ba4a84bc649fb6abb492b865a4e8700586
Kewal <panch013@umn.edu>
2016-09-28 11:50:29 -0500

    updated fioreplay

---
+++ fioreplay/fioinbatch.sh
@@ -0,0 +1,41 @@
+sudo fio mix.fio --read_iolog=FioInputTrace/fio-sdb-hibachi-1K-src2_0.trace >> ResultBandWidth/fio-sdb-hibachi-1K-src2_0.bw
+sudo fio mix.fio --read_iolog=FioInputTrace/fio-sdb-hibachi-2K-src2_0.trace >> ResultBandWidth/fio-sdb-hibachi-2K-src2_0.bw
+sudo fio mix.fio --read_iolog=FioInputTrace/fio-sdb-hibachi-4K-src2_0.trace >> ResultBandWidth/fio-sdb-hibachi-4K-src2_0.bw
+sudo fio mix.fio --read_iolog=FioInputTrace/fio-sdb-hibachi-8K-src2_0.trace >> ResultBandWidth/fio-sdb-hibachi-8K-src2_0.bw
+sudo fio mix.fio --read_iolog=FioInputTrace/fio-sdb-hibachi-16K-src2_0.trace >> ResultBandWidth/fio-sdb-hibachi-16K-src2_0.bw
+sudo fio mix.fio --read_iolog=FioInputTrace/fio-sdb-hibachi-32K-src2_0.trace >> ResultBandWidth/fio-sdb-hibachi-32K-src2_0.bw
+
+sudo fio mix.fio --read_iolog=FioInputTrace/fio-sdb-hybrid-fixed-1K-src2_0.trace >> ResultBandWidth/fio-sdb-hybrid-fixed-1K-src2_0.bw
+sudo fio mix.fio --read_iolog=FioInputTrace/fio-sdb-hybrid-fixed-2K-src2_0.trace >> ResultBandWidth/fio-sdb-hybrid-fixed-2K-src2_0.bw
+sudo fio mix.fio --read_iolog=FioInputTrace/fio-sdb-hybrid-fixed-4K-src2_0.trace >> ResultBandWidth/fio-sdb-hybrid-fixed-4K-src2_0.bw
+sudo fio mix.fio --read_iolog=FioInputTrace/fio-sdb-hybrid-fixed-8K-src2_0.trace >> ResultBandWidth/fio-sdb-hybrid-fixed-8K-src2_0.bw
+sudo fio mix.fio --read_iolog=FioInputTrace/fio-sdb-hybrid-fixed-16K-src2_0.trace >> ResultBandWidth/fio-sdb-hybrid-fixed-16K-src2_0.bw
+sudo fio mix.fio --read_iolog=FioInputTrace/fio-sdb-hybrid-fixed-32K-src2_0.trace >> ResultBandWidth/fio-sdb-hybrid-fixed-32K-src2_0.bw
+
+sudo fio mix.fio --read_iolog=FioInputTrace/fio-sdb-hybrid-dynamic-1K-src2_0.trace >> ResultBandWidth/fio-sdb-hybrid-dynamic-1K-src2_0.bw
+sudo fio mix.fio --read_iolog=FioInputTrace/fio-sdb-hybrid-dynamic-2K-src2_0.trace >> ResultBandWidth/fio-sdb-hybrid-dynamic-2K-src2_0.bw
+sudo fio mix.fio --read_iolog=FioInputTrace/fio-sdb-hybrid-dynamic-4K-src2_0.trace >> ResultBandWidth/fio-sdb-hybrid-dynamic-4K-src2_0.bw
+sudo fio mix.fio --read_iolog=FioInputTrace/fio-sdb-hybrid-dynamic-8K-src2_0.trace >> ResultBandWidth/fio-sdb-hybrid-dynamic-8K-src2_0.bw
+sudo fio mix.fio --read_iolog=FioInputTrace/fio-sdb-hybrid-dynamic-16K-src2_0.trace >> ResultBandWidth/fio-sdb-hybrid-dynamic-16K-src2_0.bw
+sudo fio mix.fio --read_iolog=FioInputTrace/fio-sdb-hybrid-dynamic-32K-src2_0.trace >> ResultBandWidth/fio-sdb-hybrid-dynamic-32K-src2_0.bw
+
+sudo fio mix.fio --read_iolog=FioInputTrace/fio-sdc-hibachi-1K-src2_0.trace >> ResultBandWidth/fio-sdc-hibachi-1K-src2_0.bw
+sudo fio mix.fio --read_iolog=FioInputTrace/fio-sdc-hibachi-2K-src2_0.trace >> ResultBandWidth/fio-sdc-hibachi-2K-src2_0.bw
+sudo fio mix.fio --read_iolog=FioInputTrace/fio-sdc-hibachi-4K-src2_0.trace >> ResultBandWidth/fio-sdc-hibachi-4K-src2_0.bw
+sudo fio mix.fio --read_iolog=FioInputTrace/fio-sdc-hibachi-8K-src2_0.trace >> ResultBandWidth/fio-sdc-hibachi-8K-src2_0.bw
+sudo fio mix.fio --read_iolog=FioInputTrace/fio-sdc-hibachi-16K-src2_0.trace >> ResultBandWidth/fio-sdc-hibachi-16K-src2_0.bw
+sudo fio mix.fio --read_iolog=FioInputTrace/fio-sdc-hibachi-32K-src2_0.trace >> ResultBandWidth/fio-sdc-hibachi-32K-src2_0.bw
+
+sudo fio mix.fio --read_iolog=FioInputTrace/fio-sdc-hybrid-fixed-1K-src2_0.trace >> ResultBandWidth/fio-sdc-hybrid-fixed-1K-src2_0.bw
+sudo fio mix.fio --read_iolog=FioInputTrace/fio-sdc-hybrid-fixed-2K-src2_0.trace >> ResultBandWidth/fio-sdc-hybrid-fixed-2K-src2_0.bw
+sudo fio mix.fio --read_iolog=FioInputTrace/fio-sdc-hybrid-fixed-4K-src2_0.trace >> ResultBandWidth/fio-sdc-hybrid-fixed-4K-src2_0.bw
+sudo fio mix.fio --read_iolog=FioInputTrace/fio-sdc-hybrid-fixed-8K-src2_0.trace >> ResultBandWidth/fio-sdc-hybrid-fixed-8K-src2_0.bw
+sudo fio mix.fio --read_iolog=FioInputTrace/fio-sdc-hybrid-fixed-16K-src2_0.trace >> ResultBandWidth/fio-sdc-hybrid-fixed-16K-src2_0.bw
+sudo fio mix.fio --read_iolog=FioInputTrace/fio-sdc-hybrid-fixed-32K-src2_0.trace >> ResultBandWidth/fio-sdc-hybrid-fixed-32K-src2_0.bw
+
+sudo fio mix.fio --read_iolog=FioInputTrace/fio-sdc-hybrid-dynamic-1K-src2_0.trace >> ResultBandWidth/fio-sdc-hybrid-dynamic-1K-src2_0.bw
+sudo fio mix.fio --read_iolog=FioInputTrace/fio-sdc-hybrid-dynamic-2K-src2_0.trace >> ResultBandWidth/fio-sdc-hybrid-dynamic-2K-src2_0.bw
+sudo fio mix.fio --read_iolog=FioInputTrace/fio-sdc-hybrid-dynamic-4K-src2_0.trace >> ResultBandWidth/fio-sdc-hybrid-dynamic-4K-src2_0.bw
+sudo fio mix.fio --read_iolog=FioInputTrace/fio-sdc-hybrid-dynamic-8K-src2_0.trace >> ResultBandWidth/fio-sdc-hybrid-dynamic-8K-src2_0.bw
+sudo fio mix.fio --read_iolog=FioInputTrace/fio-sdc-hybrid-dynamic-16K-src2_0.trace >> ResultBandWidth/fio-sdc-hybrid-dynamic-16K-src2_0.bw
+sudo fio mix.fio --read_iolog=FioInputTrace/fio-sdc-hybrid-dynamic-32K-src2_0.trace >> ResultBandWidth/fio-sdc-hybrid-dynamic-32K-src2_0.bw
---
+++ fioreplay/tracegenerateinbatch.sh
@@ -0,0 +1,41 @@
+./disksimtrace2fiotrace.sh /dev/sdb DiskSimInputTrace/DiskSimInputTrace-policy_hibachi-cachesize_1K-tracename_src2_0.trace FioInputTrace/fio-sdb-hibachi-1K-src2_0.trace
+./disksimtrace2fiotrace.sh /dev/sdb DiskSimInputTrace/DiskSimInputTrace-policy_hibachi-cachesize_2K-tracename_src2_0.trace FioInputTrace/fio-sdb-hibachi-2K-src2_0.trace
+./disksimtrace2fiotrace.sh /dev/sdb DiskSimInputTrace/DiskSimInputTrace-policy_hibachi-cachesize_4K-tracename_src2_0.trace FioInputTrace/fio-sdb-hibachi-4K-src2_0.trace
+./disksimtrace2fiotrace.sh /dev/sdb DiskSimInputTrace/DiskSimInputTrace-policy_hibachi-cachesize_8K-tracename_src2_0.trace FioInputTrace/fio-sdb-hibachi-8K-src2_0.trace
+./disksimtrace2fiotrace.sh /dev/sdb DiskSimInputTrace/DiskSimInputTrace-policy_hibachi-cachesize_16K-tracename_src2_0.trace FioInputTrace/fio-sdb-hibachi-16K-src2_0.trace
+./disksimtrace2fiotrace.sh /dev/sdb DiskSimInputTrace/DiskSimInputTrace-policy_hibachi-cachesize_32K-tracename_src2_0.trace FioInputTrace/fio-sdb-hibachi-32K-src2_0.trace
+
+./disksimtrace2fiotrace.sh /dev/sdb DiskSimInputTrace/DiskSimInputTrace-policy_hybrid-fixed-cachesize_1K-tracename_src2_0.trace FioInputTrace/fio-sdb-hybrid-fixed-1K-src2_0.trace
+./disksimtrace2fiotrace.sh /dev/sdb DiskSimInputTrace/DiskSimInputTrace-policy_hybrid-fixed-cachesize_2K-tracename_src2_0.trace FioInputTrace/fio-sdb-hybrid-fixed-2K-src2_0.trace
+./disksimtrace2fiotrace.sh /dev/sdb DiskSimInputTrace/DiskSimInputTrace-policy_hybrid-fixed-cachesize_4K-tracename_src2_0.trace FioInputTrace/fio-sdb-hybrid-fixed-4K-src2_0.trace
+./disksimtrace2fiotrace.sh /dev/sdb DiskSimInputTrace/DiskSimInputTrace-policy_hybrid-fixed-cachesize_8K-tracename_src2_0.trace FioInputTrace/fio-sdb-hybrid-fixed-8K-src2_0.trace
+./disksimtrace2fiotrace.sh /dev/sdb DiskSimInputTrace/DiskSimInputTrace-policy_hybrid-fixed-cachesize_16K-tracename_src2_0.trace FioInputTrace/fio-sdb-hybrid-fixed-16K-src2_0.trace
+./disksimtrace2fiotrace.sh /dev/sdb DiskSimInputTrace/DiskSimInputTrace-policy_hybrid-fixed-cachesize_32K-tracename_src2_0.trace FioInputTrace/fio-sdb-hybrid-fixed-32K-src2_0.trace
+
+./disksimtrace2fiotrace.sh /dev/sdb DiskSimInputTrace/DiskSimInputTrace-policy_hybrid-dynamic-cachesize_1K-tracename_src2_0.trace FioInputTrace/fio-sdb-hybrid-dynamic-1K-src2_0.trace
+./disksimtrace2fiotrace.sh /dev/sdb DiskSimInputTrace/DiskSimInputTrace-policy_hybrid-dynamic-cachesize_2K-tracename_src2_0.trace FioInputTrace/fio-sdb-hybrid-dynamic-2K-src2_0.trace
+./disksimtrace2fiotrace.sh /dev/sdb DiskSimInputTrace/DiskSimInputTrace-policy_hybrid-dynamic-cachesize_4K-tracename_src2_0.trace FioInputTrace/fio-sdb-hybrid-dynamic-4K-src2_0.trace
+./disksimtrace2fiotrace.sh /dev/sdb DiskSimInputTrace/DiskSimInputTrace-policy_hybrid-dynamic-cachesize_8K-tracename_src2_0.trace FioInputTrace/fio-sdb-hybrid-dynamic-8K-src2_0.trace
+./disksimtrace2fiotrace.sh /dev/sdb DiskSimInputTrace/DiskSimInputTrace-policy_hybrid-dynamic-cachesize_16K-tracename_src2_0.trace FioInputTrace/fio-sdb-hybrid-dynamic-16K-src2_0.trace
+./disksimtrace2fiotrace.sh /dev/sdb DiskSimInputTrace/DiskSimInputTrace-policy_hybrid-dynamic-cachesize_32K-tracename_src2_0.trace FioInputTrace/fio-sdb-hybrid-dynamic-32K-src2_0.trace
+
+./disksimtrace2fiotrace.sh /dev/sdc DiskSimInputTrace/DiskSimInputTrace-policy_hibachi-cachesize_1K-tracename_src2_0.trace FioInputTrace/fio-sdc-hibachi-1K-src2_0.trace
+./disksimtrace2fiotrace.sh /dev/sdc DiskSimInputTrace/DiskSimInputTrace-policy_hibachi-cachesize_2K-tracename_src2_0.trace FioInputTrace/fio-sdc-hibachi-2K-src2_0.trace
+./disksimtrace2fiotrace.sh /dev/sdc DiskSimInputTrace/DiskSimInputTrace-policy_hibachi-cachesize_4K-tracename_src2_0.trace FioInputTrace/fio-sdc-hibachi-4K-src2_0.trace
+./disksimtrace2fiotrace.sh /dev/sdc DiskSimInputTrace/DiskSimInputTrace-policy_hibachi-cachesize_8K-tracename_src2_0.trace FioInputTrace/fio-sdc-hibachi-8K-src2_0.trace
+./disksimtrace2fiotrace.sh /dev/sdc DiskSimInputTrace/DiskSimInputTrace-policy_hibachi-cachesize_16K-tracename_src2_0.trace FioInputTrace/fio-sdc-hibachi-16K-src2_0.trace
+./disksimtrace2fiotrace.sh /dev/sdc DiskSimInputTrace/DiskSimInputTrace-policy_hibachi-cachesize_32K-tracename_src2_0.trace FioInputTrace/fio-sdc-hibachi-32K-src2_0.trace
+
+./disksimtrace2fiotrace.sh /dev/sdc DiskSimInputTrace/DiskSimInputTrace-policy_hybrid-fixed-cachesize_1K-tracename_src2_0.trace FioInputTrace/fio-sdc-hybrid-fixed-1K-src2_0.trace
+./disksimtrace2fiotrace.sh /dev/sdc DiskSimInputTrace/DiskSimInputTrace-policy_hybrid-fixed-cachesize_2K-tracename_src2_0.trace FioInputTrace/fio-sdc-hybrid-fixed-2K-src2_0.trace
+./disksimtrace2fiotrace.sh /dev/sdc DiskSimInputTrace/DiskSimInputTrace-policy_hybrid-fixed-cachesize_4K-tracename_src2_0.trace FioInputTrace/fio-sdc-hybrid-fixed-4K-src2_0.trace
+./disksimtrace2fiotrace.sh /dev/sdc DiskSimInputTrace/DiskSimInputTrace-policy_hybrid-fixed-cachesize_8K-tracename_src2_0.trace FioInputTrace/fio-sdc-hybrid-fixed-8K-src2_0.trace
+./disksimtrace2fiotrace.sh /dev/sdc DiskSimInputTrace/DiskSimInputTrace-policy_hybrid-fixed-cachesize_16K-tracename_src2_0.trace FioInputTrace/fio-sdc-hybrid-fixed-16K-src2_0.trace
+./disksimtrace2fiotrace.sh /dev/sdc DiskSimInputTrace/DiskSimInputTrace-policy_hybrid-fixed-cachesize_32K-tracename_src2_0.trace FioInputTrace/fio-sdc-hybrid-fixed-32K-src2_0.trace
+
+./disksimtrace2fiotrace.sh /dev/sdc DiskSimInputTrace/DiskSimInputTrace-policy_hybrid-dynamic-cachesize_1K-tracename_src2_0.trace FioInputTrace/fio-sdc-hybrid-dynamic-1K-src2_0.trace
+./disksimtrace2fiotrace.sh /dev/sdc DiskSimInputTrace/DiskSimInputTrace-policy_hybrid-dynamic-cachesize_2K-tracename_src2_0.trace FioInputTrace/fio-sdc-hybrid-dynamic-2K-src2_0.trace
+./disksimtrace2fiotrace.sh /dev/sdc DiskSimInputTrace/DiskSimInputTrace-policy_hybrid-dynamic-cachesize_4K-tracename_src2_0.trace FioInputTrace/fio-sdc-hybrid-dynamic-4K-src2_0.trace
+./disksimtrace2fiotrace.sh /dev/sdc DiskSimInputTrace/DiskSimInputTrace-policy_hybrid-dynamic-cachesize_8K-tracename_src2_0.trace FioInputTrace/fio-sdc-hybrid-dynamic-8K-src2_0.trace
+./disksimtrace2fiotrace.sh /dev/sdc DiskSimInputTrace/DiskSimInputTrace-policy_hybrid-dynamic-cachesize_16K-tracename_src2_0.trace FioInputTrace/fio-sdc-hybrid-dynamic-16K-src2_0.trace
+./disksimtrace2fiotrace.sh /dev/sdc DiskSimInputTrace/DiskSimInputTrace-policy_hybrid-dynamic-cachesize_32K-tracename_src2_0.trace FioInputTrace/fio-sdc-hybrid-dynamic-32K-src2_0.trace
